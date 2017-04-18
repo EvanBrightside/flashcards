c7a7d34ae870f6c6aa08263b81fe35c14aa7c686
EvanBrightside <xvanx84@gmail.com>
2017-04-18 11:14:40 +0300

    Fix quotes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
 require 'open-uri'
 
-doc = Nokogiri::HTML(open("http://1000mostcommonwords.com/1000-most-common-russian-words/"))
+doc = Nokogiri::HTML(open('http://1000mostcommonwords.com/1000-most-common-russian-words/'))
 
-cards_list = doc.xpath('.//tbody/tr[position() > 1]')
-  .map { |a| [a.xpath('td[position() = 2]').text, a.xpath('td[position() = 3]').text] }
+cards_list = doc.xpath('.//tbody/tr[position() > 1]').map { |a|
+  [a.xpath('td[position() = 2]').text, a.xpath('td[position() = 3]').text]
+}
 
 cards_list.each do |original, translate|
   Card.create( original_text: original, translated_text: translate )
