cf5bc3308766ebd08533a56c4669b09cc6030da7
EvanBrightside <xvanx84@gmail.com>
2017-04-21 15:49:57 +0300

    Add logic for swhoing cards on main page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 # Initial controller for root page
 class HomeController < ApplicationController
   def index
+    @card = Card.sample_card.sample
+    @card_origianl = @card.original_text
+  end
+
+  def perform
+    if  params[:home][:translated_text] == Card.find(params[:home][:id]).translated_text
+      Card.find(params[:home][:id]).update(review_date: Time.now + 3.days)
+      flash[:message] = 'Yep!'
+      redirect_to :back
+    else
+      flash[:error] = 'No!'
+      redirect_to :back
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root "home#index"
 
   resources :cards
+  resources :home
+
+  post "/" => "home#perform"
 end
