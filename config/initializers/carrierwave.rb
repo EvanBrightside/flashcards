b620529cb74686d43824aff52b8844cd185841a3
EvanBrightside <xvanx84@gmail.com>
2017-09-09 03:26:11 +0300

    fix space indentions

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,15 +4,15 @@ if Rails.env.test?
     config.enable_processing = false
   end
 else
-	CarrierWave.configure do |config|
-	  config.fog_provider = 'fog/aws'
-	  config.fog_credentials = {
-	      provider:              'AWS',
-	      aws_access_key_id:     ENV['S3_KEY'],
-	      aws_secret_access_key: ENV['S3_SECRET'],
-	      region:                ENV['S3_REGION'],
-	  }
-	  config.fog_directory  = ENV['S3_BUCKET']             # required
+  CarrierWave.configure do |config|
+    config.fog_provider = 'fog/aws'
+    config.fog_credentials = {
+        provider:              'AWS',
+        aws_access_key_id:     ENV['S3_KEY'],
+        aws_secret_access_key: ENV['S3_SECRET'],
+        region:                ENV['S3_REGION'],
+    }
+    config.fog_directory  = ENV['S3_BUCKET']             # required
     config.fog_public     = true                             # optional, defaults to true
     config.fog_attributes = { cache_control: "public, max-age=#{365.day.to_i}" } # optional, defaults to {}
   end
