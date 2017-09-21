e06152cad0d93b6d610e797acd29dabe1ff62e02
EvanBrightside <xvanx84@gmail.com>
2017-09-22 00:05:28 +0300

    fix callbacks in sorcery

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -100,20 +100,20 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.twitter.key = ENV['TWITTER_KEY']
   config.twitter.secret = ENV['TWITTER_SECRET']
-  config.twitter.callback_url = 'http://0.0.0.0:3000/oauth/callback?provider=twitter'
+  config.twitter.callback_url = ENV['TWITTER_CALLBACK_URL']
   config.twitter.user_info_mapping = { email: 'email', name: 'name' }
   config.twitter.user_info_path = "/1.1/account/verify_credentials.json?include_email=true"
   #
   config.facebook.key = ENV['FACEBOOK_KEY']
   config.facebook.secret = ENV['FACEBOOK_SECRET']
-  config.facebook.callback_url = 'http://0.0.0.0:3000/oauth/callback?provider=facebook'
+  config.facebook.callback_url = ENV['FACEBOOK_CALLBACK_URL']
   config.facebook.user_info_mapping = { email: 'email', name: 'name' }
   config.facebook.scope = "email,public_profile"
   config.facebook.user_info_path = "me?fields=email,name"
   #
   config.github.key = ENV['GITHUB_KEY']
   config.github.secret = ENV['GITHUB_SECRET']
-  config.github.callback_url = 'http://0.0.0.0:3000/oauth/callback?provider=github'
+  config.github.callback_url = ENV['GITHUB_CALLBACK_URL']
   config.github.user_info_mapping = { email: 'email', name: 'name' }
   #
   # config.paypal.key = ""
@@ -143,7 +143,7 @@ Rails.application.config.sorcery.configure do |config|
   #
   config.vk.key = ENV['VK_KEY']
   config.vk.secret = ENV['VK_SECRET']
-  config.vk.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=vk"
+  config.vk.callback_url = ENV['VK_CALLBACK_URL']
   config.vk.user_info_mapping = { email: 'email', name: 'full_name' }
   #
   # config.slack.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=slack"
