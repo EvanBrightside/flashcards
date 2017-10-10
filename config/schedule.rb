a51f3526fd8347e90312aa45e6ff21067abc39fd
EvanBrightside <xvanx84@gmail.com>
2017-10-10 10:53:24 +0300

    test cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,9 @@
 #set :output, 'log/whenever.log'
 
-every 1.day, at: '5:30 am' do
+every 2.minutes do
   runner 'Card.pending_cards_notification'
 end
+
+# every 1.day, at: '5:30 am' do
+#   runner 'Card.pending_cards_notification'
+# end
