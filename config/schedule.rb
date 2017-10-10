969fef071f8c57eb714fd198d55cd72ad93e8a24
EvanBrightside <xvanx84@gmail.com>
2017-10-10 11:37:35 +0300

    fix shedule

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,5 @@
 #set :output, 'log/whenever.log'
 
-every 2.minutes do
+every 1.day, at: '5:30 am' do
   runner 'Card.pending_cards_notification'
 end
-
-# every 1.day, at: '5:30 am' do
-#   runner 'Card.pending_cards_notification'
-# end
