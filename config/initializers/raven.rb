5a69198b79cdd63cca3e04174f17386fc1fefea9
EvanBrightside <xvanx84@gmail.com>
2018-03-11 02:24:03 +0300

    set sentry url

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,4 +1,3 @@
 Raven.configure do |config|
-  config.dsn = ENV['SENTRY_URL']
-  config.environments = ['production']
+  config.dsn = 'https://5726d58082c54e11a4f5706f314815a4:93007aed3aa544078b6e9ecaff6d4241@sentry.io/301595'
 end
