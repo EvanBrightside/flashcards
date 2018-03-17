9db1de5a6754d2524c232d198840420f11b262e1
EvanBrightside <xvanx84@gmail.com>
2018-03-17 22:12:56 +0300

    move format js to index

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,11 @@ class HomeController < ApplicationController
     else
       @card = current_user.cards.sample_card.sample
     end
-    return if @card
+    respond_to do |format|
+      format.html
+      format.js
+    end
+    return if  @card
     redirect_to cards_path
     flash[:error] = t('cards.cards_done')
   end
@@ -16,9 +20,7 @@ class HomeController < ApplicationController
     answer = params[:home][:translated_text]
     message = CheckAnswer.new(@card, answer).check
     flash[:message] = message
-    respond_to do |format|
-      format.html { redirect_back(fallback_location: root_path) }
-      format.js
-    end
+
+    redirect_to home_index_path
   end
 end
