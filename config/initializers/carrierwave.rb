fff6ff808dfff6dc9f52cf38237fe711a859b55e
EvanBrightside <xvanx84@gmail.com>
2018-03-19 13:33:44 +0300

    unlock aws keys

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
       provider:              'AWS',
-      aws_access_key_id:     ENV['S3_KEY'],
-      aws_secret_access_key: ENV['S3_SECRET'],
+      aws_access_key_id:     AKIAJ6X6UZGXWUINARPQ,
+      aws_secret_access_key: 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,
       region:                ENV['S3_REGION']
   }
   config.fog_directory  = ENV['S3_BUCKET']
